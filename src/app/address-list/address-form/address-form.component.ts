f4a3a95ffc45cba23719baeb2c986ef2aa212b23
dnwebdesign <luca@dn-webdesign.de>
2023-04-15 18:44:54 +0200

    minor refactoring

--- src/app/address-list/address-form/address-form.component.ts
+++ src/app/address-list/address-form/address-form.component.ts
@@ -1,7 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {Address} from "../../shared/address.model";
 import {AddressService} from "../../shared/address.service";
-import {AddressInput} from "../../shared/address-input";
 import {Router} from "@angular/router";
 import {AddressFormValidation} from '../../shared/address-form-validation';
 
@@ -12,7 +11,6 @@ import {AddressFormValidation} from '../../shared/address-form-validation';
     styleUrls: ['./address-form.component.scss'],
 })
 export class AddressFormComponent {
-    newAddress: AddressInput = this.createEmptyAddressInput();
     @Output() closeAddressForm = new EventEmitter<void>();
     submitted = false;
     validation: AddressFormValidation = new AddressFormValidation();
@@ -22,7 +20,7 @@ export class AddressFormComponent {
 
     addAddress(): void {
         this.submitted = true;
-        if (this.validation.isValid() && this.newAddress) {
+        if (this.validation.isValid()) {
             const id = this.addressService.generateAddressId();
             const address = new Address(
                 id,
@@ -35,20 +33,7 @@ export class AddressFormComponent {
                 <string>this.validation.locationControl.value,
             )
             this.addressService.addAddress(address);
-            this.newAddress = this.createEmptyAddressInput();
             this.router.navigate(['/']);
         }
-    }
-
-    private createEmptyAddressInput(): AddressInput {
-        return {
-            name: '',
-            phone: '',
-            mail: '',
-            street: '',
-            streetNo: '',
-            zip: '',
-            location: ''
-        };
     }
 }

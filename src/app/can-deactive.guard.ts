950a4f613a25b99eb0bf98ea654794330c1efabb
dnwebdesign <luca@dn-webdesign.de>
2023-04-15 20:06:49 +0200

    code refactoring

--- src/app/can-deactive.guard.ts
+++ src/app/can-deactive.guard.ts
@@ -1,16 +1,15 @@
 import {Injectable} from '@angular/core';
 import {CanDeactivate} from '@angular/router';
-import {Observable} from 'rxjs';
 
 export interface CanComponentDeactivate {
-    canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+    canDeactivate: () => boolean;
 }
 
 @Injectable({
     providedIn: 'root',
 })
 export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
-    canDeactivate(component: CanComponentDeactivate): Observable<boolean> | Promise<boolean> | boolean {
+    canDeactivate(component: CanComponentDeactivate): boolean {
         return component.canDeactivate ? component.canDeactivate() : true;
     }
 }
